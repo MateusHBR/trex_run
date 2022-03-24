fa55e069190f38ced39b51524141b468f2b1e0df
MateusHBR <mateusmorais.r@gmail.com>
2022-03-24 00:30:20 -0300

    import assets

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+const kDino = 'dino/dino.png';
+const kDinoGif = 'dino/dino.gif';
+
+const List<String> kJungleParalax = [
+  '/scenario/paralax/plx-1.png',
+  '/scenario/paralax/plx-2.png',
+  '/scenario/paralax/plx-3.png',
+  '/scenario/paralax/plx-4.png',
+  '/scenario/paralax/plx-5.png',
+];
